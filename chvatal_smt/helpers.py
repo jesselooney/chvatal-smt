a05c74c92d461c87127f74239401ac6c5ce0b288
Jesse Looney 27 <jlooney27@hpc-login2.cm.cluster>
2025-04-24 14:20:35 -0400

    add exact ZIMPL copy for opt

--- chvatal_smt/helpers.py
+++ chvatal_smt/helpers.py
@@ -8,3 +8,9 @@ def powerset(iterable: Iterable) -> Iterable:
     # powerset([1,2,3]) → () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
+
+
+def subsets(iterable: Iterable, n, m) -> Iterable:
+    """Subsequences of the iterable from shortest to longest, with lengths in [n, m]"""
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(n, m + 1))
